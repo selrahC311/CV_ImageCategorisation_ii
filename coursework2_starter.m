5a5872713f279fd42e2d7d0001d22964b863f6b1
caleb <66700303+caleb-adeyemo@users.noreply.github.com>
2023-05-08 15:55:26 +0100

    Update coursework2_starter.m

--- coursework2_starter.m
+++ coursework2_starter.m
@@ -1,27 +1,43 @@
+% Michal Mackiewicz, UEA 
+% This code has been adapted from the code 
+% prepared by James Hays, Brown University
+
 %% Step 0: Set up parameters, vlfeat, category list, and image paths.
 
-% FEATURE = 'bag of sift grayscale';
-FEATURE = 'bag of sift colour';
-step = 3;
-size = 8;
-
-% FEATURE = 'spatial pyramids sift grayscale'
-% FEATURE = 'spatial pyramids sift colour'
+% FEATURE = 'tiny image';
+% FEATURE = 'colour histogram';
+FEATURE = "bag of sift";
 
 CLASSIFIER = 'nearest neighbor';
-% CLASSIFIER = 'support vector machines';
+% CLASSIFIER = "svm";
 
-data_path = 'data/';
+% Set up paths to VLFeat functions. 
+% See http://www.vlfeat.org/matlab/matlab.html for VLFeat Matlab documentation
+% This should work on 32 and 64 bit versions of Windows, MacOS, and Linux
+%run('vlfeat/toolbox/vl_setup')
 
+data_path = '../data/';
+
+%This is the list of categories / directories to use. The categories are
+%somewhat sorted by similarity so that the confusion matrix looks more
+%structured (indoor and then urban and then rural).
 categories = {'Kitchen', 'Store', 'Bedroom', 'LivingRoom', 'House', ...
        'Industrial', 'Stadium', 'Underwater', 'TallBuilding', 'Street', ...
        'Highway', 'Field', 'Coast', 'Mountain', 'Forest'};
-
+   
+%This list of shortened category names is used later for visualization.
 abbr_categories = {'Kit', 'Sto', 'Bed', 'Liv', 'Hou', 'Ind', 'Sta', ...
     'Und', 'Bld', 'Str', 'HW', 'Fld', 'Cst', 'Mnt', 'For'};
     
+%number of training examples per category to use. Max is 100. For
+%simplicity, we assume this is the number of test cases per category, as
+%well.
 num_train_per_cat = 100; 
 
+%This function returns cell arrays containing the file path for each train
+%and test image, as well as cell arrays with the label of each train and
+%test image. By default all four of these arrays will be 1500x1 where each
+%entry is a char array (or string).
 fprintf('Getting paths and labels for all train and test data\n')
 [train_image_paths, test_image_paths, train_labels, test_labels] = ...
     get_image_paths(data_path, categories, num_train_per_cat);
@@ -31,79 +47,99 @@ fprintf('Getting paths and labels for all train and test data\n')
 %   test_labels        1500x1   cell          
 
 %% Step 1: Represent each image with the appropriate feature
+% Each function to construct features should return an N x d matrix, where
+% N is the number of paths passed to the function and d is the 
+% dimensionality of each image representation. See the starter code for
+% each function for more details.
 
 fprintf('Using %s representation for images\n', FEATURE)
 
 switch lower(FEATURE)    
-    case 'bag of sift grayscale'
+    case 'tiny image'
+        %You need to reimplement get_tiny_images. Allow function to take
+        %parameters e.g. feature size.
+        
+        % image_paths is an N x 1 cell array of strings where each string is an
+        %  image path on the file system.
+        % image_feats is an N x d matrix of resized and then vectorized tiny
+        %  images. E.g. if the images are resized to 16x16, d would equal 256.
+        
+        % To build a tiny image feature, simply resize the original image to a very
+        % small square resolution, e.g. 16x16. You can either resize the images to
+        % square while ignoring their aspect ratio or you can crop the center
+        % square portion out of each image. Making the tiny images zero mean and
+        % unit length (normalizing them) will increase performance modestly.
+        
+        train_image_feats = get_tiny_images(train_image_paths);
+        test_image_feats  = get_tiny_images(test_image_paths);
+    case 'colour histogram'
+        %You should allow get_colour_histograms to take parameters e.g.
+        %quantisation, colour space etc.
+        train_image_feats = get_colour_histograms(train_image_paths);
+        test_image_feats  = get_colour_histograms(test_image_paths);
+     case 'bag of sift'
         % YOU CODE build_vocabulary.m
-        % if ~exist('vocab.mat', 'file')
+        if ~exist('vocab.mat', 'file')
             fprintf('No existing dictionary found. Computing one from training images\n')
-            vocab_size = 51; % you need to test the influence of this parameter
+            vocab_size = 1000; % you need to test the influence of this parameter
             vocab = build_vocabulary(train_image_paths, vocab_size); %Also allow for different sift parameters
             save('vocab.mat', 'vocab')
-        % end 
+        end
+        
         % YOU CODE get_bags_of_sifts.m
-        % if ~exist('image_feats.mat', 'file')
-            train_image_feats = get_bag_of_sifts_grayscale(train_image_paths, step, size); %Allow for different sift parameters
-            test_image_feats  = get_bag_of_sifts_grayscale(test_image_paths, step, size); 
-            save('image_feats.mat', 'train_image_feats', 'test_image_feats')
-        % end
-    
-    case 'bag of sift colour'
-        % YOU CODE build_vocabulary.m
-        % if ~exist('vocab.mat', 'file')
-            fprintf('No existing dictionary found. Computing one from training images\n')
-            vocab_size = 50; % you need to test the influence of this parameter
-            vocab = build_vocabulary(train_image_paths, vocab_size); %Also allow for different sift parameters
-            save('vocab.mat', 'vocab')
-        % end 
-        % YOU CODE get_bags_of_sifts.m
-        % if ~exist('image_feats.mat', 'file')
-            train_image_feats = get_bag_of_sifts_colour(train_image_paths, step, size); %Allow for different sift parameters
-            test_image_feats  = get_bag_of_sifts_colour(test_image_paths, step, size); 
-            save('image_feats.mat', 'train_image_feats', 'test_image_feats')
-        % end
-    
-    case 'spatial pyramids grayscale'
-        % YOU CODE build_vocabulary.m
-        % if ~exist('vocab.mat', 'file')
-            fprintf('No existing dictionary found. Computing one from training images\n')
-            vocab_size = 50; % you need to test the influence of this parameter
-            vocab = build_vocabulary(train_image_paths, vocab_size); %Also allow for different sift parameters
-            save('vocab.mat', 'vocab')
-        % end 
-        % YOU CODE get_bags_of_sifts.m
-        % if ~exist('image_feats.mat', 'file')
-            train_image_feats = get_spatial_pyramid_grayscale(train_image_paths, step, size); %Allow for different sift parameters
-            test_image_feats  = get_spatial_pyramid_grayscale(test_image_paths, step, size); 
-            save('image_feats.mat', 'train_image_feats', 'test_image_feats')
-        % end
-
-    case 'spatial pyramids colour'
-       % YOU CODE build_vocabulary.m
-        % if ~exist('vocab.mat', 'file')
-            fprintf('No existing dictionary found. Computing one from training images\n')
-            vocab_size = 50; % you need to test the influence of this parameter
-            vocab = build_vocabulary(train_image_paths, vocab_size); %Also allow for different sift parameters
-            save('vocab.mat', 'vocab')
-        % end 
-        % YOU CODE get_bags_of_sifts.m
-        % if ~exist('image_feats.mat', 'file')
-            train_image_feats = get_spatial_pyramid_colour(train_image_paths, step, size); %Allow for different sift parameters
-            test_image_feats  = get_spatial_pyramid_colour(test_image_paths, step, size); 
-            save('image_feats.mat', 'train_image_feats', 'test_image_feats')
-        % end
+        if ~exist('training_bag.mat', 'file')
+            fprintf('Computing training features\n');
+            train_image_feats = get_bags_of_sifts(train_image_paths);
+            save('training_bag.mat', 'train_image_feats');
+        else
+            fprintf('Loading training features\n');
+            load('training_bag.mat');
+        end
+        
+        if ~exist('test_bag.mat', 'file')
+            fprintf('Computing test features\n');
+            test_image_feats  = get_bags_of_sifts(test_image_paths);
+            save('test_bag.mat', 'test_image_feats')
+        else
+            fprintf('Loading test features\n');
+            load('test_bag.mat');
+        end
+      case 'spatial pyramids'
+          % YOU CODE spatial pyramids method
 end
 %% Step 2: Classify each test image by training and using the appropriate classifier
+% Each function to classify test features will return an N x 1 cell array,
+% where N is the number of test cases and each entry is a string indicating
+% the predicted category for each test image. Each entry in
+% 'predicted_categories' must be one of the 15 strings in 'categories',
+% 'train_labels', and 'test_labels'. See the starter code for each function
+% for more details.
+
 fprintf('Using %s classifier to predict test set categories\n', CLASSIFIER)
 
 switch lower(CLASSIFIER)    
     case 'nearest neighbor'
-        % Useful functions: pdist2 (Matlab) and vl_alldist2 (from vlFeat toolbox)
-        predicted_categories = nearest_neighbor_classify(1, train_image_feats, train_labels, test_image_feats);
+    %Here, you need to reimplement nearest_neighbor_classify. My P-code
+    %implementation has k=1 set. You need to allow for varying this
+    %parameter.
+        
+    %This function will predict the category for every test image by finding
+    %the training image with most similar features. Instead of 1 nearest
+    %neighbor, you can vote based on k nearest neighbors which will increase
+    %performance (although you need to pick a reasonable value for k).
+    
+    % image_feats is an N x d matrix, where d is the dimensionality of the
+    %  feature representation.
+    % train_labels is an N x 1 cell array, where each entry is a string
+    %  indicating the ground truth category for each training image.
+    % test_image_feats is an M x d matrix, where d is the dimensionality of the
+    %  feature representation. You can assume M = N unless you've modified the
+    %  starter code.
+    % predicted_categories is an M x 1 cell array, where each entry is a string
+    %  indicating the predicted category for each test image.
+    % Useful functions: pdist2 (Matlab) and vl_alldist2 (from vlFeat toolbox)
+        predicted_categories = knn_classifier(1, train_image_feats, train_labels, test_image_feats);
     case 'support vector machine'
-        % TODO svm 
         predicted_categories = svm_classify(train_image_feats, train_labels, test_image_feats);
 end
 
