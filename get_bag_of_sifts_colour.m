5cd8c88c6ba1ed724d09183f49b7b4655af6df70
selrahC311 <charles.yt.tsang@gmail.com>
2023-05-19 04:14:37 +0100

    modified bag of sifts compatibility with color vocab

--- get_bag_of_sifts_colour.m
+++ get_bag_of_sifts_colour.m
@@ -1,6 +1,5 @@
-function [image_feats] = get_bag_of_sifts_colour(image_paths, step, size_)
-
-load('vocab.mat')
+function [image_feats] = get_bag_of_sifts_colour(image_paths, step, size_, vocab_path)
+load(vocab_path);
 
 % Find out how many images we are processing
 total_image = size(image_paths, 1);
@@ -13,29 +12,27 @@ image_feats = zeros(total_image, vocab_size);
 
 % Loop theough every image in the file
 for image_count = 1:total_image
-
     % Read the image and turn it into grayscale
     image = imread(cell2mat(image_paths(image_count)));
     
     % Make the image of type single to work with the function vl_dsift()
     image = single(image);
 
-    indices_image = [];
+    descriptors = [];
     for channel = 1:3
         colour_channel = image(:, :, channel);
         
         % Extract the SIFT Features and Descriptors
-        [~, descriptors] = vl_dsift(colour_channel, 'step', step, 'size', size_, 'Fast');
+        [~, channel_descriptors] = vl_dsift(colour_channel, 'step', step, 'size', size_, 'Fast');
         
-        % Convert the descriptors to single data type
-        descriptors = single(descriptors);
-        
-        % Find the nearrest vocab to each descriptor found
-        [indices, ~] = knnsearch(vocab, descriptors', "K", 1);
-        
-        % Concat each channel to single feature
-        indices_image = cat(1, indices_image, indices);
+        % Concatenate the descriptors for the current channel
+        descriptors = [descriptors, channel_descriptors];
     end
-    
-    image_feats(image_count, :) = histcounts(indices_image, vocab_size);
+
+    % Find the nearrest vocab to each descriptor found
+    [indices, ~] = knnsearch(vocab, single(descriptors'), "K", 1);
+    image_feats(image_count, :) = histcounts(indices, vocab_size);
+
+    % Normalize histogram
+    image_feats(image_count, :) = image_feats(image_count, :) / sum(image_feats(image_count, :));
 end
